2adf0f4a77bcd817caef26bb5bddc053b09a19d9
Kirill <twenztergames@gmail.com>
2026-07-29 15:12:29 +0300

    Настроить checstyle 2.0

--- src/test/java/ru/yandex/practicum/filmorate/FilmServiceTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmServiceTest.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
